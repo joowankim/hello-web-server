7f279a120d1cdd8197039c0814cf9b229102b2a6
Joowan Kim <rlawndhks217@gmail.com>
2025-06-12 11:51:21 +0900

    r: simplify regex pattern

--- python-web-server/web_server/http/message.py
+++ python-web-server/web_server/http/message.py
@@ -1,4 +1,4 @@
 import re
 
 RFC9110_5_6_2_TOKEN_SPECIALS = r"!#$%&'*+-.^_`|~"
-TOKEN_PATTERN = re.compile(rf"[{re.escape(RFC9110_5_6_2_TOKEN_SPECIALS)}0-9a-zA-Z]+")
+TOKEN_PATTERN = re.compile(rf"[{re.escape(RFC9110_5_6_2_TOKEN_SPECIALS)}\w]+")
