873e8f58bbf1299fbbb9201382595ad15f167e49
Joowan Kim <rlawndhks217@gmail.com>
2025-06-21 11:32:43 +0900

    c: add `RequestBody.read` method

--- python-web-server/tests/test_request_body.py
+++ python-web-server/tests/test_request_body.py
@@ -116,3 +116,49 @@ def test_create_with_invalid_request(
             headers=headers,
             socket_reader=socket_reader,
         )
+
+
+@pytest.fixture
+def request_body(request: pytest.FixtureRequest) -> RequestBody:
+    payload: bytes = request.param
+    fake_sock = fake.FakeSocket(payload)
+    fake_sock = cast(socket.socket, fake_sock)
+    return RequestBody.create(
+        protocol_version=(1, 1),
+        headers=[("Content-Length", str(len(payload)))],
+        socket_reader=SocketReader(sock=fake_sock, max_chunk=8192),
+    )
+
+
+@pytest.mark.parametrize(
+    "request_body, size, expected",
+    [
+        (b"Hello, World!", None, b"Hello, World!"),
+        (b"Hello, World!", 5, b"Hello"),
+        (b"Hello, World!", 13, b"Hello, World!"),
+        (b"Hello, World!", 0, b""),
+        (b"", None, b""),
+        (b"", 0, b""),
+        (b"", 5, b""),
+    ],
+    indirect=["request_body"],
+)
+def test_read(request_body: RequestBody, size: int | None, expected: bytes):
+    data = request_body.read(size)
+
+    assert data == expected
+
+
+@pytest.mark.parametrize(
+    "request_body, sizes, expected_list",
+    [
+        (b"Hello, World!", [5, 5, 5, 5], [b"Hello", b", Wor", b"ld!", b""]),
+        (b"", [0, 5], [b"", b""]),
+    ],
+    indirect=["request_body"],
+)
+def test_read_iteration(
+    request_body: RequestBody, sizes: list[int | None], expected_list: list[bytes]
+):
+    for size, expected in zip(sizes, expected_list):
+        assert request_body.read(size) == expected
