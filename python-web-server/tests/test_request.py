e9603edd0c98f149163cc72e8f6b790673d232ab
Joowan Kim <rlawndhks217@gmail.com>
2025-06-26 10:44:42 +0900

    u: use `RequestParser` to initiate `Request`

--- python-web-server/tests/test_request.py
+++ python-web-server/tests/test_request.py
@@ -4,26 +4,40 @@ from typing import cast
 import pytest
 
 from tests import fake
+from web_server.config import MessageConfig
+from web_server.http.parser import RequestParser
 from web_server.http.reader import SocketReader
 from web_server.request import Request
 
 
 @pytest.fixture
-def socket_reader(request: pytest.FixtureRequest) -> SocketReader:
-    payload: bytes = request.param
-    fake_sock = fake.FakeSocket(payload)
-    fake_sock = cast(socket.socket, fake_sock)
-    return SocketReader(sock=fake_sock, max_chunk=8192)
+def message_config() -> MessageConfig:
+    return MessageConfig.custom(
+        limit_request_line=4094,
+        limit_request_fields=100,
+        limit_request_field_size=8190,
+    )
+
+
+@pytest.fixture
+def request_parser(
+    message_config: MessageConfig, request: pytest.FixtureRequest
+) -> RequestParser:
+    payload = request.param
+    sock = fake.FakeSocket(payload)
+    sock = cast(socket.socket, sock)
+    socket_reader = SocketReader(sock, max_chunk=8192)
+    return RequestParser(message_config, socket_reader)
 
 
 @pytest.mark.parametrize(
-    "socket_reader, expected",
+    "request_parser, expected",
     [
         (
             b"GET / HTTP/1.1\r\nHost: example.com\r\nContent-Length: 13\r\n\r\nHello, World!",
             (
                 "GET",
-                "/",
+                ("/", "", ""),
                 [("Host", "example.com"), ("Content-Length", "13")],
                 b"Hello, World!",
             ),
@@ -32,20 +46,20 @@ def socket_reader(request: pytest.FixtureRequest) -> SocketReader:
             b"POST /submit HTTP/1.1\r\nHost: example.com\r\nContent-Length: 0\r\n\r\n",
             (
                 "POST",
-                "/submit",
+                ("/submit", "", ""),
                 [("Host", "example.com"), ("Content-Length", "0")],
                 b"",
             ),
         ),
         (
             b"PUT /update HTTP/1.0\r\nHost: example.com\r\n\r\n",
-            ("PUT", "/update", [("Host", "example.com")], b""),
+            ("PUT", ("/update", "", ""), [("Host", "example.com")], b""),
         ),
         (
             b"DELETE /delete HTTP/1.1\r\nHost: example.com\r\nContent-Length: 0\r\n\r\n",
             (
                 "DELETE",
-                "/delete",
+                ("/delete", "", ""),
                 [("Host", "example.com"), ("Content-Length", "0")],
                 b"",
             ),
@@ -54,21 +68,24 @@ def socket_reader(request: pytest.FixtureRequest) -> SocketReader:
             b"POST /upload HTTP/1.1\r\nHost: example.com\r\nTransfer-Encoding: chunked\r\n\r\n5\r\nHello\r\n3\r\n, W\r\n5\r\norld!\r\n0\r\n\r\n",
             (
                 "POST",
-                "/upload",
+                ("/upload", "", ""),
                 [("Host", "example.com"), ("Transfer-Encoding", "chunked")],
                 b"Hello, World!",
             ),
         ),
     ],
-    indirect=["socket_reader"],
+    indirect=["request_parser"],
 )
 def test_create(
-    socket_reader: SocketReader, expected: tuple[str, str, list[tuple[str, str]], bytes]
+    request_parser: RequestParser,
+    expected: tuple[str, str, list[tuple[str, str]], bytes],
 ):
-    req = Request.create(socket_reader)
+    req = Request.create(request_parser=request_parser)
 
-    method, url, headers, body = expected
+    method, (path, query, fragment), headers, body = expected
     assert req.method == method
-    assert req.url == url
+    assert req.path == path
+    assert req.query == query
+    assert req.fragment == fragment
     assert req.headers == headers
     assert req.body.read() == body
--- python-web-server/web_server/request.py
+++ python-web-server/web_server/request.py
@@ -1,55 +1,40 @@
-import re
 from collections.abc import Sequence
-from typing import Self, ClassVar
+from typing import Self
 
-from web_server.http import reader
+from web_server.http import reader, parser
 from web_server.http.body import RequestBody
-from web_server.http.errors import InvalidHTTPVersion
 
 
 class Request:
-    PROTOCOL_VERSION_TOKEN: ClassVar[re.Pattern] = re.compile(
-        r"HTTP/(?P<major>\d+)\.(?P<minor>\d+)"
-    )
-
     def __init__(
         self,
         method: str,
-        url: str,
+        path: str,
+        query: str,
+        fragment: str,
         headers: Sequence[tuple[str, str]],
         body: RequestBody,
     ):
         self.method = method
-        self.url = url
+        self.path = path
+        self.query = query
+        self.fragment = fragment
         self.headers = headers
         self.body = body
 
     @classmethod
-    def create(cls, socket_reader: reader.SocketReader) -> Self:
-        line = socket_reader.read_until(b"\r\n")
-        if not line:
-            raise ValueError("Empty request line")
+    def create(cls, request_parser: parser.RequestParser) -> Self:
+        method, (path, query, fragment), version = request_parser.parse_request_line()
+        headers = request_parser.parse_headers()
 
-        parts = line.decode().split(" ")
-        if len(parts) < 3:
-            raise ValueError("Invalid request line")
-
-        method, url, version = parts[0], parts[1], parts[2]
-        matched = cls.PROTOCOL_VERSION_TOKEN.match(version)
-        if not matched:
-            raise InvalidHTTPVersion(version)
-        version = (int(matched.group("major")), int(matched.group("minor")))
-        headers = []
-
-        while True:
-            header_line = socket_reader.read_until(b"\r\n")
-            if header_line == b"\r\n":
-                break
-            header_parts = header_line.decode().strip().split(": ", 1)
-            if len(header_parts) != 2:
-                raise ValueError("Invalid header format")
-            headers.append((header_parts[0], header_parts[1]))
-
-        body = RequestBody.create(version, headers, socket_reader)
-
-        return cls(method=method, url=url, headers=headers, body=body)
+        body = RequestBody.create(version, headers, request_parser.reader)
+        if isinstance(body.reader, reader.ChunkedReader):
+            headers += body.reader.trailers
+        return cls(
+            method=method,
+            path=path,
+            query=query,
+            fragment=fragment,
+            headers=headers,
+            body=body,
+        )
